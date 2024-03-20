c60331ed2a439b68bc15d514cf26467b225907e6
Tony Troeff <tonyslav_troeff@outlook.com>
2024-03-20 08:38:34 +0200

    Publishing a solution of the `Top Integers` problem form the `Arrays` lecture of `C++ Advanced` using bitwise operations.

--- C++ Fundamentals/Arrays/Top Integers/Top Integers.cpp
+++ C++ Fundamentals/Arrays/Top Integers/Top Integers.cpp
@@ -2,13 +2,25 @@
 #include <limits.h>
 using namespace std;
 
+constexpr int MAX_N = 100;
+
 void readArray(int arr[], const int n) {
     for (int i = 0; i < n; i++) cin >> arr[i];
 }
 
+/*
+// Required for "Solution #3"
+constexpr int BITMASK_ENTRY_LENGTH = sizeof(int) * 8;
+constexpr int BITMASK_ENTRIES = MAX_N / sizeof(int);
+
+void findBitmaskIndices(const int& position, int& bitmaskEntryIndex, int& bitIndex) {
+    bitmaskEntryIndex = position / BITMASK_ENTRY_LENGTH;
+    bitIndex = position % BITMASK_ENTRY_LENGTH;
+}
+*/
+
 int main()
 {
-    constexpr int MAX_N = 100;
     int arr[MAX_N];
 
     int n;
@@ -16,6 +28,7 @@ int main()
 
     readArray(arr, n);
 
+    /*
     // Solution #1, Complexity: O(n^2), Simple
     for (int i = 0; i < n; i++) {
         bool isTop = true;
@@ -28,6 +41,7 @@ int main()
 
         if (isTop) cout << arr[i] << ' ';
     }
+    */
 
     /*
     // Solution #2, Complexity: O(n), Advanced
@@ -41,6 +55,30 @@ int main()
 
     for (int i = 0; i < n; i++) {
         if (topElements[i]) cout << arr[i] << ' ';
+    }
+    */
+
+    /*
+    // Solution #3, Complexity: O(n), Advanced
+    int bitmask[BITMASK_ENTRIES]{};
+    int runningMax = INT_MIN;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] > runningMax) {
+            int bitmaskEntryIndex, bitIndex;
+            findBitmaskIndices(i, bitmaskEntryIndex, bitIndex);
+
+            bitmask[bitmaskEntryIndex] |= 1 << bitIndex;
+            runningMax = arr[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int bitmaskEntryIndex, bitIndex;
+        findBitmaskIndices(i, bitmaskEntryIndex, bitIndex);
+
+        if (bitmask[bitmaskEntryIndex] & (1 << bitIndex))
+            cout << arr[i] << ' ';
     }
     */
 
