2259c65f1790c685de0545d0f07c0e44fc68127a
Tony Troeff <tonyslav_troeff@outlook.com>
2024-07-22 23:47:31 +0300

    Solved the `Pets` problem from the `More Exercises` section for the `Conditional Statements` lecture of the `Programming Basics with C#` course

---
+++ Programming Basics/C#/Conditional Statements/More Exercises/Pets/Program.cs
@@ -0,0 +1,9 @@
+﻿int days = int.Parse(Console.ReadLine());
+int leftFoodKg = int.Parse(Console.ReadLine());
+double dogFoodKg = double.Parse(Console.ReadLine());
+double catFoodKg = double.Parse(Console.ReadLine());
+double turtleFoodKg = double.Parse(Console.ReadLine()) / 1000;
+
+double requiredFood = days * (dogFoodKg + catFoodKg + turtleFoodKg);
+if (leftFoodKg >= requiredFood) Console.WriteLine($"{Math.Floor(leftFoodKg - requiredFood)} kilos of food left.");
+else Console.WriteLine($"{Math.Ceiling(requiredFood - leftFoodKg)} more kilos of food are needed.");
