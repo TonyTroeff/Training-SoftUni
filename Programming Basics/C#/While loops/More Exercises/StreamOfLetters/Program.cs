74a6626d03448fb43f7145b84bab73d25bd51437
Tony Troeff <tonyslav_troeff@outlook.com>
2024-08-09 20:31:35 +0300

    Solved the `Stream Of Letters` problem from the `While loops` lecture of the `Programming Basics with C#` course

---
+++ Programming Basics/C#/While loops/More Exercises/StreamOfLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿bool cIsFound = false, oIsFound = false, nIsFound = false;
+string currentWord = "";
+
+string input = Console.ReadLine();
+while (input != "End")
+{
+    char symbol = input[0];
+    if (char.IsLetter(symbol))
+    {
+        if (symbol == 'c' && !cIsFound) cIsFound = true;
+        else if (symbol == 'o' && !oIsFound) oIsFound = true;
+        else if (symbol == 'n' && !nIsFound) nIsFound = true;
+        else currentWord += symbol;
+
+        if (cIsFound && oIsFound && nIsFound)
+        {
+            Console.Write(currentWord);
+            Console.Write(' ');
+
+            cIsFound = false;
+            oIsFound = false;
+            nIsFound = false;
+            currentWord = "";
+        }
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine();
