3f96ae6f1cc9be4d42024c73834e59d904474511
Tony Troeff <tonyslav_troeff@outlook.com>
2025-04-27 14:22:13 +0300

    Publishing solutions for all exercises from the `Java DB Apps Introduction` lecture of the `Spring Data` course.

---
+++ Spring Data/Java DB Apps Introduction/Exercises/Exercise7.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        int[] minionIds = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", "root", "root")) {
+            updateMinions(connection, minionIds);
+            printAllMinions(connection);
+        }
+    }
+
+    private static void updateMinions(Connection connection, int[] minionIds) throws SQLException {
+        String idPlaceholders = String.join(", ", Collections.nCopies(minionIds.length, "?"));
+        String updateQuery = String.format("update minions as m set m.age = m.age + 1, m.name = lower(m.name) where m.id in (%s)", idPlaceholders);
+
+        try (PreparedStatement statement = connection.prepareStatement(updateQuery)) {
+            for (int i = 0; i < minionIds.length; i++) statement.setInt(i + 1, minionIds[i]);
+            statement.executeUpdate();
+        }
+    }
+
+    private static void printAllMinions(Connection connection) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("select m.name, m.age from minions as m")) {
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) {
+                    String name = result.getString("name");
+                    int age = result.getInt("age");
+
+                    System.out.printf("%s %d%n", name, age);
+                }
+            }
+        }
+    }
+}
---
+++ Spring Data/Java DB Apps Introduction/Exercises/Exercise8.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        int minionId = Integer.parseInt(scanner.nextLine());
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", "root", "root")) {
+            updateMinion(connection, minionId);
+            printMinion(connection, minionId);
+        }
+    }
+
+    private static void updateMinion(Connection connection, int minionId) throws SQLException {
+        // The braces are not required for MySql.
+        try (CallableStatement statement = connection.prepareCall("{call usp_get_older(?)}")) {
+            statement.setInt(1, minionId);
+            statement.execute();
+        }
+    }
+
+    private static void printMinion(Connection connection, int minionId) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("select m.name, m.age from minions as m where m.id = ?")) {
+            statement.setInt(1, minionId);
+
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    String name = result.getString("name");
+                    int age = result.getInt("age");
+
+                    System.out.printf("%s %d%n", name, age);
+                }
+            }
+        }
+    }
+}
